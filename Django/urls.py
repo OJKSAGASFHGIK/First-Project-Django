e2c5090de68ffe15ef5f8b20ea49fec0bb024dc5
OJKSAGASFHGIK <ichijou_raku@hotmail.com>
2022-11-02 21:27:56 -0300

    origin/another-link

--- Django/urls.py
+++ Django/urls.py
@@ -1,4 +1,4 @@
-"""Project URL Configuration
+"""Django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def solitacao(request):
+    return HttpResponse('Coffee with banana')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('texto/', solitacao),
 ]
--- history commands.py
+++ history commands.py
@@ -1,5 +1,18 @@
 """
-Terminal:
++Python:
+-urls.py:
+return HttpRequest() (Django module)
+"""
+"""
++Django:
+-base commands:
+from django.contrib import admin
+from django.urls import path
+-extra commands:
+from django.http import HttpRequest
+"""
+"""
+-Terminal:
 'ctrl+click to open the link'
 python manage.py runserver (every code you use, have manage.py include)
 django-admin startproject NameThatYouWantPut .
