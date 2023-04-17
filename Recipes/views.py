4df1e03ded41825953a5eeaba9f808cae8488250
Greque <mgsouzacampos@hotmail.com>
2023-04-16 21:30:03 -0300

    request fixed

--- Recipes/views.py
+++ Recipes/views.py
@@ -2,15 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def nothing(ItsArequest):
+def nothing(request):
     return HttpResponse('Hello... what do you want?!!')
 
 
-def contact(YeapArequest):
+def contact(request):
     return HttpResponse('My discord is Greque#7432')
 
 
-def about(OhYesARequestttt):
+def about(request):
     return HttpResponse('I like coffee!')
 
 # Create your views here.
