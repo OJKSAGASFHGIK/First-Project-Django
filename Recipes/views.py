7647051afab246d66545a5842ff703399e51a816
Greque <mgsouzacampos@hotmail.com>
2023-06-04 11:18:55 -0300

    1.0

--- Recipes/views.py
+++ Recipes/views.py
@@ -1,9 +1,19 @@
+from utils.recipes.factory import make_recipe
+
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/a1pages/a1home.html', context={
-        'name': 'Recipes'
+        'name': 'Recipes',
+        'recipes': [make_recipe() for _ in range(6)],
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/a1pages/a2recipeView.html', context={
+        'recipe': make_recipe(),
+        'a1': True,  # texto grandão
     })
 
 # Create your views here.
